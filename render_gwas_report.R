eeb93552ef73fe5e307213595043b549dfa86f61
YiLiu6240 <yi.liu6240@gmail.com>
2018-12-05 11:13:12 +0000

    Modularise parser

--- render_gwas_report.R
+++ render_gwas_report.R
@@ -1,37 +1,37 @@
-PARSER <- argparse::ArgumentParser()
-PARSER$add_argument(
-  "-g", "--gwas_id",
-  type = "character", default = "example",
-  help = "Directory with the associated gwas_id [default: %(default)s]")
-PARSER$add_argument(
-  "-i", "--input",
-  type = "character", default = "harmonised.bcf",
-  help = "Input bcf file [default: %(default)s]")
-PARSER$add_argument(
-  "-m", "--metadata",
-  default = "harmonised.json", type = "character",
-  help = "metadata json file: [default %(default)s]")
-PARSER$add_argument(
-  "-q", "--quietly",
-  type = "logical", default = TRUE,
-  help = "If not quietly, will open the report file. [default: %(default)s]")
-ARGS <- PARSER$parse_args()
+suppressPackageStartupMessages({
+  library("tidyverse")
+  library("glue")
+  library("fs")
+  source("funcs/processing.R")
+  source("funcs/plots.R")
+})
 
-library("tidyverse")
-library("glue")
-library("fs")
-source("funcs/processing.R")
-source("funcs/plots.R")
+get_args <- function() {
+  parser <- argparse::ArgumentParser()
+  parser$add_argument(
+    "--gwas_id",
+    type = "character", default = "example",
+    help = "Directory with the associated gwas_id [default: %(default)s]")
+  parser$add_argument(
+    "--input",
+    type = "character", default = "harmonised.bcf",
+    help = "Input bcf file [default: %(default)s]")
+  parser$add_argument(
+    "--metadata",
+    default = "harmonised.json", type = "character",
+    help = "metadata json file: [default %(default)s]")
+  parser$add_argument(
+    "-s", "--show",
+    action = "store_true", default = TRUE,
+    help = "If True, show the report after it is generated")
+  args <- parser$parse_args()
+  return(args)
+}
 
-main <- function(gwas_id, bcf_file, metadata, quietly) {
-  if (FALSE) {
-    gwas_id <- "example"
-    bcf_file <- "harmonised.bcf"
-    metadata <- "harmonised.json"
-  }
+main <- function(gwas_id, input, metadata, show) {
   # Sanitise paths
   gwas_dir <- path("gwas_input", gwas_id)
-  bcf_file <- path(gwas_dir, path_file(bcf_file))
+  bcf_file <- path(gwas_dir, path_file(input))
   metadata <- path(gwas_dir, path_file(metadata))
   report_file <- glue("report_{gwas_id}.html")
   report_full_path <- path(gwas_dir, report_file)
@@ -79,7 +79,7 @@ main <- function(gwas_id, bcf_file, metadata, quietly) {
                   metadata = metadata))
 
   if (file_exists(report_full_path)) {
-    if (quietly) {
+    if (!show) {
       message(glue(
         "{Sys.time()}\tSuccess!! (～o￣▽￣)～[]\n",
         "Report available at {report_full_path}."))
@@ -92,7 +92,4 @@ main <- function(gwas_id, bcf_file, metadata, quietly) {
 
 }
 
-main(gwas_id = ARGS$gwas_id,
-     bcf_file = ARGS$input,
-     metadata = ARGS$metadata,
-     quietly = ARGS$quietly)
+do.call(main, get_args())
