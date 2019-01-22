7a37b8e5c416e6c64e65b430d62697b2066b1233
YiLiu6240 <yi.liu6240@gmail.com>
2019-01-22 15:00:40 +0000

    Refactor headers and use csv format

--- funcs/processing.R
+++ funcs/processing.R
@@ -9,57 +9,71 @@ process_bcf_file <- function(bcf_file, intermediates_dir, ref_file,
   #' - `reuse`, lgl: if TRUE, reuse the already processed file
   #' - `clean_intermediates`, lgl: if TRUE, clean up intermediate files
   #'                               of the various stages
-  proc <- function(bcf_file, intermediates_dir, ref_file, tsv_file,
+  proc <- function(bcf_file, intermediates_dir, ref_file, csv_file,
                    clean_intermediates = TRUE) {
+    stage1_bcf_header <- paste("%CHROM", "%POS", "%ID",
+                               "%INFO/B", "%INFO/SE",
+                               "%INFO/PVAL", "%INFO/AF",
+                               sep = ",")
+    stage1_csv_header <- c("CHROM", "POS", "ID",
+                           "BETA", "SE",
+                           "PVAL", "AF")
+    stage1_csv_col_types <- "cicdddd"
+    stage2_bcf_header <- paste("%CHROM", "%POS", "%ID",
+                               "%INFO/AF",
+                               sep = ",")
+    stage2_csv_header <- c("CHROM", "POS", "ID", "AF_reference")
+    stage2_csv_col_types <- "cicd"
     # Stage 1: Extract from input data
-    stage1_tsv_file <- path(intermediates_dir, "report_query_stage1.tsv")
+    stage1_csv_file <- path(intermediates_dir, "report_query_stage1.csv")
     stage1_cmd <- glue(
       "bcftools query",
-      " -f '%CHROM\t%POS\t%ID\t%INFO/PVAL\t%INFO/AF\n'",
-      " {bcf_file} > {stage1_tsv_file}")
+      " -f '{stage1_bcf_header}\n'",
+      " {bcf_file} > {stage1_csv_file}")
     message(glue("{Sys.time()}\tcmd: {stage1_cmd}"))
     system(stage1_cmd)
     # Stage 2: Extract from reference data
-    stage2_tsv_file <- path(intermediates_dir, "report_query_stage2.tsv")
+    stage2_csv_file <- path(intermediates_dir, "report_query_stage2.csv")
     stage2_cmd <- glue(
       "bcftools query",
-      " -f '%CHROM\t%POS\t%ID\t%INFO/AF\n'",
-      " {ref_file} > {stage2_tsv_file}")
+      " -f '{stage2_bcf_header}\n'",
+      " {ref_file} > {stage2_csv_file}")
     message(glue("{Sys.time()}\tcmd: {stage2_cmd}"))
     system(stage2_cmd)
     # Stage3: Join
-    stage1_df <- read_tsv(stage1_tsv_file, col_names = FALSE,
-                          col_types = "cicdd",
+    stage1_df <- read_csv(stage1_csv_file, col_names = FALSE,
+                          col_types = stage1_csv_col_types,
                           na = c("", "NA", ".")) %>%
-      set_names(c("CHROM", "POS", "ID", "PVAL", "AF"))
-    stage2_df <- read_tsv(stage2_tsv_file, col_names = FALSE,
-                          col_types = "cicd",
+      set_names(stage1_csv_header)
+    stage2_df <- read_csv(stage2_csv_file, col_names = FALSE,
+                          col_types = stage2_csv_col_types,
                           na = c("", "NA", ".")) %>%
-      set_names(c("CHROM", "POS", "ID", "AF_reference"))
+      set_names(stage2_csv_header)
     joined_df <- stage1_df %>%
       left_join(stage2_df, by = c("CHROM", "POS", "ID")) %>%
       mutate_at(vars(CHROM), translate_chrom_to_int)
-    message(glue("{Sys.time()}\tcaching to: {tsv_file}"))
-    joined_df %>% write_tsv(tsv_file)
+    message(glue("{Sys.time()}\tcaching to: {csv_file}"))
+    joined_df %>% write_csv(csv_file)
     # Finally, clean up
     if (clean_intermediates) {
       message(glue("{Sys.time()}\tRemoving intermediates:",
-                   " {stage1_tsv_file}",
-                   " {stage2_tsv_file}"))
-      file_delete(stage1_tsv_file)
-      file_delete(stage2_tsv_file)
+                   " {stage1_csv_file}",
+                   " {stage2_csv_file}"))
+      file_delete(stage1_csv_file)
+      file_delete(stage2_csv_file)
     }
   }
 
-  tsv_file <- path(intermediates_dir, "report_query_combined.tsv")
-  if (file_exists(tsv_file) && reuse) {
-    message(glue("{Sys.time()}\treusing file: {tsv_file}"))
+  csv_col_types <- "iicddddd"
+  csv_file <- path(intermediates_dir, "report_query_combined.csv")
+  if (file_exists(csv_file) && reuse) {
+    message(glue("{Sys.time()}\treusing file: {csv_file}"))
   } else {
-    proc(bcf_file, intermediates_dir, ref_file, tsv_file,
+    proc(bcf_file, intermediates_dir, ref_file, csv_file,
          clean_intermediates = clean_intermediates)
   }
 
-  read_tsv(tsv_file, col_types = "iicddd")
+  read_csv(csv_file, col_types = csv_col_types)
 }
 
 translate_chrom_to_int <- function(chrom_series) {
