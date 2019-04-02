cf0b125f3e439e24f81769f04cf503224c57352a
YiLiu6240 <yi.liu6240@gmail.com>
2019-04-02 21:48:20 +0100

    Add automated meta plots and tables

--- funcs/meta_plots.R
+++ funcs/meta_plots.R
@@ -5,6 +5,18 @@ plot_scatter <- function(df, x, y) {
     ggplot(.) +
       aes(x = !!x, y = !!y) +
       geom_point(color = "#8ec07c", alpha = 0.5) +
-      theme_minimal()
+      theme_classic() +
+      theme(axis.text.x = element_blank(), axis.ticks.x = element_blank())
   }
+}
+
+plot_hist <- function(df, x) {
+  x <- enquo(x)
+  df %>% {
+    ggplot(.) +
+      aes(x = !!x) +
+      geom_histogram(fill = "#8ec07c", alpha = 0.5, stat = "count") +
+      theme_classic()
+  }
+
 }
